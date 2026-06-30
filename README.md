38f3f40745ed7a7f1be7c63e44c567fef53d6ae4
victorxaviercosta <151531232+victorxaviercosta@users.noreply.github.com>
2026-06-30 09:19:35 -0300

    roteiro_3

---
+++ README.md
@@ -0,0 +1,12 @@
+# Roteiro III — Criação de modelos e relacionamento com DJango
+
+Execução do terceiro roteiro prático da disciplina de Programação Web
+
+---
+### Sistema em execução:
+
+![Sistema em Execução (index)](figures/roteiro_3_index.png)
+
+![Sistema em Execução (admin - mensagens)](figures/roteiro_3_admin_messages.png)
+
+![Sistema em Execução (admin - categorias)](figures/roteiro_3_admin_category.png)
--- home/models.py
+++ home/models.py
@@ -1,11 +1,28 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
 class Message(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField()
     author = models.CharField(max_length=80, default="Anônimo")
     creation_timestamp = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="messages"
+    )
 
     class Meta:
         ordering = ["-creation_timestamp"]
